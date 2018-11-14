7810957c51e453a774d240d46df2518418443201
Michael Arranaga <Michael.Arranaga@unt.edu>
2018-11-14 13:31:46 -0600

    Cleared out Write-Host

--- Tests/Public/Version/Get-GLVersion.Tests.ps1
+++ Tests/Public/Version/Get-GLVersion.Tests.ps1
@@ -1,22 +1,14 @@
 [String]           $ModuleName = 'Posh-GitLab'
-Write-Host ('Module name: {0}' -f $ModuleName)
 [String]           $ProjectDirectoryName = 'Version'
-Write-Host ('Project Directory Name: {0}' -f $ProjectDirectoryName)
 [IO.FileInfo]      $PesterFile = [IO.FileInfo] ([String] (Resolve-Path -Path $MyInvocation.MyCommand.Path))
-Write-Host ('Pester File: {0}' -f $PesterFile)
 [IO.DirectoryInfo] $ProjectRoot = Split-Path $pesterFile.Directory -Parent
 While ((Split-Path $ProjectRoot.FullName -Leaf) -ne $ModuleName) {
     $ProjectRoot = Split-Path $ProjectRoot.FullName -Parent
 }
-Write-Host ('Project Root: {0}' -f $ProjectRoot)
 [IO.DirectoryInfo] $PrivateDirectory = ([String] (Join-Path (Join-Path -Path $ProjectRoot.FullName -ChildPath 'Posh-GitLab' -Resolve) -ChildPath 'Private' -Resolve))
-Write-Host ('Private Directory: {0}' -f $PrivateDirectory)
 [IO.DirectoryInfo] $ProjectDirectory = ([String] (Get-ChildItem (Join-Path -Path $ProjectRoot.FullName -ChildPath 'Posh-GitLab' -Resolve) -Filter $ProjectDirectoryName -Directory -Recurse).FullName)
-Write-Host ('Project Directory: {0}' -f $ProjectDirectory)
 [IO.DirectoryInfo] $ExampleDirectory = ([String] (Get-ChildItem (Join-Path -Path $ProjectRoot.FullName -ChildPath 'Examples' -Resolve) -Filter (($PesterFile.Name).Split('.')[0]) -Directory -Recurse).FullName)
-Write-Host ('Example Directory: {0}' -f $ExampleDirectory)
 [IO.FileInfo] $TestFile = Join-Path -Path $ProjectDirectory -ChildPath ($PesterFile.Name -replace '\.Tests\.', '.') -Resolve
-Write-Host ('Test File: {0}' -f $TestFile)
 
 . $TestFile.FullName
 Get-ChildItem $PrivateDirectory | % { . $_.FullName }
